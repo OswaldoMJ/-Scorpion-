e1084c190c8b050c0af78c3b02f055be0d4dd36a
OswaldoMJ <waltermatul123@gmail.com>
2023-02-14 09:51:30 -0600

    final

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Nombre: Walter Oswaldo Matul Jeatas, Carnet: 1562923")
+ 
+ 
+import os 
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt','w')
+    archivo.write('Hola mundo')
+    archivo.close() 
